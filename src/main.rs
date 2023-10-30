2a6c2e5b08dcba3a4c0f2db77de36f3ae2582a82
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-10-30 10:50:14 -0300

    wow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::collections::LinkedList;
+use std::{
+    collections::LinkedList,
+    env::Args,
+    io::{stdin, stdout},
+    time::Instant,
+};
 
 const PTABLE: [&str; 118] = [
     "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl",
@@ -11,13 +16,24 @@ const PTABLE: [&str; 118] = [
     "Fl", "Mc", "Lv", "Ts", "Og",
 ];
 
+const PTABLE_LOWER: [&str; 118] = [
+    "h", "he", "li", "be", "b", "c", "n", "o", "f", "ne", "na", "mg", "al", "si", "p", "s", "cl",
+    "ar", "k", "ca", "sc", "ti", "v", "cr", "mn", "fe", "co", "ni", "cu", "zn", "ga", "ge", "as",
+    "se", "br", "kr", "rb", "sr", "y", "zr", "nb", "mo", "tc", "ru", "rh", "pd", "ag", "cd", "in",
+    "sn", "sb", "te", "i", "xe", "cs", "ba", "la", "ce", "pr", "nd", "pm", "sm", "eu", "gd", "tb",
+    "dy", "ho", "er", "tm", "yb", "lu", "hf", "ta", "w", "re", "os", "ir", "pt", "au", "hg", "tl",
+    "pb", "bi", "po", "at", "rn", "fr", "ra", "ac", "th", "pa", "u", "np", "pu", "am", "cm", "bk",
+    "cf", "es", "fm", "md", "no", "lr", "rf", "db", "sg", "bh", "hs", "mt", "ds", "rg", "cn", "nh",
+    "fl", "mc", "lv", "ts", "og",
+];
+
 fn message_to_periodic(message: &str) -> Option<LinkedList<&'static str>> {
     if message.is_empty() {
         return Some(LinkedList::new());
     }
 
-    for p in PTABLE {
-        if message.starts_with(&p.to_lowercase()) {
+    for (p, pl) in PTABLE.iter().zip(PTABLE_LOWER.iter()) {
+        if message.starts_with(pl) {
             if let Some(mut result) = message_to_periodic(&message[p.len()..]) {
                 result.push_front(p);
                 return Some(result);
@@ -28,17 +44,62 @@ fn message_to_periodic(message: &str) -> Option<LinkedList<&'static str>> {
     return None;
 }
 
-fn main() {
+fn remove_accent(ch: char) -> char {
+    match ch {
+        'á' => 'a',
+        'é' => 'e',
+        'í' => 'i',
+        'ó' => 'o',
+        'ú' => 'u',
+        'ã' => 'a',
+        'õ' => 'o',
+        'à' => 'a',
+        'è' => 'e',
+        'ì' => 'i',
+        'ò' => 'o',
+        'ù' => 'u',
+        'â' => 'a',
+        'ê' => 'e',
+        'î' => 'i',
+        'ô' => 'o',
+        'û' => 'u',
+        _ => ch,
+    }
+}
+
+fn trab1(args: Args) {
     let message: String = std::env::args()
         .nth(1)
         .expect("Nenhuma mensagem foi passada como argumento")
         .to_lowercase()
         .chars()
         .filter(|c| c.is_alphabetic())
+        .map(remove_accent)
         .collect();
 
+    let instant = Instant::now();
+
     match message_to_periodic(&message) {
-        Some(result) => println!("{:?}", result),
+        Some(result) => println!("{:?} em {:?}", result, instant.elapsed()),
         None => println!("Combinação não encontrada"),
     };
+}
+
+fn main() {
+    stdin()
+        .lines()
+        .flatten()
+        .map(|l| {
+            l.to_lowercase()
+                .chars()
+                .filter(|c| c.is_alphabetic())
+                .map(remove_accent)
+                .collect::<String>()
+        })
+        .map(|message| {
+            let periodic = message_to_periodic(&message)?;
+            Some((message, periodic))
+        })
+        .flatten()
+        .for_each(|(message, periodic)| println!("{} = {:?}", message, periodic));
 }
